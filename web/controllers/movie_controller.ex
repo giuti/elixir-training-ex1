e107139e3172f657bca232b31746d533184746b7
Giuti <s.valle.serrano@gmail.com>
2016-06-21 00:37:26 +0200

    Test 4 - creates resource and redirects when data is valid

--- web/controllers/movie_controller.ex
+++ web/controllers/movie_controller.ex
@@ -10,4 +10,9 @@ defmodule Ex1.MovieController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"movie" => movie_params}) do
+    Movies.insert(movie_params)
+    redirect conn, to: movie_path(conn, :index)
+  end
 end
