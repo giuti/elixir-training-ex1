7e43c2abb6652d8dcacdec59ba6a2d5129329800
Giuti <s.valle.serrano@gmail.com>
2016-06-20 23:59:18 +0200

    Test 3 - renders form for new movie

--- web/controllers/movie_controller.ex
+++ web/controllers/movie_controller.ex
@@ -6,4 +6,8 @@ defmodule Ex1.MovieController do
   def index(conn, _params) do
     render conn, "index.html", movies: Movies.all
   end
+
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
 end
